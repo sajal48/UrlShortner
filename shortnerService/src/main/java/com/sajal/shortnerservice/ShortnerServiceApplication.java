97bc1dd0e237c5f747f7456a27c21887f89a44b3
Sajal <newtonh48@gmail.com>
2025-03-14 20:11:12 +0600

    init shoretnerService

---
+++ shortnerService/src/main/java/com/sajal/shortnerservice/ShortnerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sajal.shortnerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortnerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortnerServiceApplication.class, args);
+    }
+
+}
---
+++ shortnerService/src/test/java/com/sajal/shortnerservice/ShortnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajal.shortnerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortnerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
