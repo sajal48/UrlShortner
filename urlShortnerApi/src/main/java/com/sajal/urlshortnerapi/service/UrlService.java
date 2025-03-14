c85d4d412b9b47f0b1cd4419d4e0927dcb08939d
Sajal <newtonh48@gmail.com>
2025-03-14 19:17:25 +0600

    redis added for faster fetching

--- urlShortnerApi/src/main/java/com/sajal/urlshortnerapi/service/UrlService.java
+++ urlShortnerApi/src/main/java/com/sajal/urlshortnerapi/service/UrlService.java
@@ -3,14 +3,19 @@ package com.sajal.urlshortnerapi.service;
 import com.sajal.urlshortnerapi.entity.UrlMapping;
 import com.sajal.urlshortnerapi.repository.UrlRepository;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class UrlService {
     private final UrlRepository urlRepository;
+    private final StringRedisTemplate redisTemplate;
 
-    public UrlService(UrlRepository urlRepository) {
+    public UrlService(UrlRepository urlRepository, StringRedisTemplate redisTemplate) {
         this.urlRepository = urlRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     public String createShortUrl(@NotNull String longUrl) {
@@ -20,10 +25,16 @@ public class UrlService {
         urlMapping.setLongUrl(longUrl);
         urlMapping.setClickCount(0);
         urlRepository.save(urlMapping);
+        redisTemplate.opsForValue().set("url:" + shortUrl, longUrl , 1 , TimeUnit.DAYS);
         return shortUrl;
     }
 
     public String getLongUrl(@NotNull String shortUrl){
+        String longUrl = redisTemplate.opsForValue().get("url:" + shortUrl);
+        if (longUrl != null) {
+            return longUrl;
+        }
+
         UrlMapping urlMapping = urlRepository.findById(shortUrl).orElseThrow(() -> new IllegalArgumentException("Invalid short URL"));
         return urlMapping.getLongUrl();
     }
